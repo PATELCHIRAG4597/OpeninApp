0dc21b973b7345e069b9a08fa4caaf10870c871a
PATELCHIRAG4597 <124140359+PATELCHIRAG4597@users.noreply.github.com>
2023-05-20 10:39:51 +0530

    Initial Commit

---
+++ OpeninApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpeninApp
+//
+//  Created by CodeInfoWay CodeInfoWay on 5/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
